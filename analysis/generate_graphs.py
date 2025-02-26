3d4522f2686d6c31ce59e4170b83d343fc5829cc
Ankush Gupta <ankushgupta1806@gmail.com>
2025-02-27 00:23:28 +0530

    Removed Unused Folder

--- analysis/generate_graphs.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_bar_chart(subjects, scores):
-    plt.figure(figsize=(8, 5))
-    plt.bar(subjects, scores, color='skyblue')
-    plt.xlabel('Subjects')
-    plt.ylabel('Scores')
-    plt.title('Subject-wise Scores')
-    plt.show()
--- analysis/generate_reports.py
+++
@@ -1,8 +0,0 @@
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-
-def generate_pdf_report(filename, content):
-    c = canvas.Canvas(filename, pagesize=letter)
-    width, height = letter
-    c.drawString(100, height - 100, content)
-    c.save()
